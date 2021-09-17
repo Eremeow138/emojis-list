3f3a9fbaa2b1952ae5355827bce798aca1abb4d8
Eremeow138 <eremeow138@yandex.ru>
2021-09-17 15:48:11 +0800

    feat: setup routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { TableComponent } from './components/table/table.component';
+
+const appRoutes: Routes = [
+  { path: 'table/:category', component: TableComponent },
+  { path: '', redirectTo: 'table/all', pathMatch: 'full' },
+  { path: '**', redirectTo: 'table/all' },
+];
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
+  imports: [RouterModule.forRoot(appRoutes, { useHash: true })],
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
